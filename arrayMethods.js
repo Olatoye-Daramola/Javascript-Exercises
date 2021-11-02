18e0c64fc1a58273bdeea8ec7f4dbfb529627c83
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-02 14:36:36 +0100

    ...

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,21 +1,73 @@
-let numsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+// let numsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+//
+// function multiply(x) {
+//     for (let y = 1; y <= 12; y++) {
+//         console.log(`${x} x ${y} = ${x * y}`)
+//     }
+// }
+//
+// function addition(x) {
+//     for (let y = 1; y <= 12; y++) {
+//         console.log(`${x} + ${y} = ${x + y}`)
+//     }
+// }
+//
+// for (let i = 1; i <= 12; i++) {
+//     numsArray.forEach(multiply)
+// }
+//
+// for (let i = 1; i <= 12; i++) {
+//     numsArray.forEach(addition)
+// }
 
-function multiply(x) {
-    for (let y = 1; y <= 12; y++) {
-        console.log(`${x} x ${y} = ${x * y}`)
+let objArray = [
+    {
+        gender: "male",
+        firstname: "John",
+        lastname: "Smith"
+    },
+    {
+        gender: "male",
+        firstname: "Demola",
+        lastname: "Femi"
+    },
+    {
+        gender: "male",
+        firstname: "Tife",
+        lastname: "Harry"
+    },
+    {
+        gender: "female",
+        firstname: "Kim",
+        lastname: "Yetunde"
     }
-}
+]
 
-function addition(x) {
-    for (let y = 1; y <= 12; y++) {
-        console.log(`${x} + ${y} = ${x + y}`)
-    }
-}
+// ------------------- QUESTION 1 -------------------
+objArray.forEach(function (individual) {
+    console.log(individual.firstname)
+})
 
-for (let i = 1; i <= 12; i++) {
-    numsArray.forEach(multiply)
-}
 
-for (let i = 1; i <= 12; i++) {
-    numsArray.forEach(addition)
-}
+// ------------------- QUESTION 2 -------------------
+let mappedArray = objArray.map(function (individual) {
+    individual.fullname =  individual.firstname + " " + individual.lastname
+    return individual.fullname
+})
+console.log("\nFullNames added:\n", mappedArray)
+
+
+// ------------------- QUESTION 3 -------------------
+let filteredMale = objArray.filter(function (individual) {
+    return individual.gender === "male"
+})
+
+console.log("\nMales only:\n", filteredMale)
+
+
+// ------------------- QUESTION 4 -------------------
+let filteredFemale = objArray.filter(function (individual) {
+    return individual.gender === "female"
+})
+
+console.log("\nFemales only:\n", filteredFemale)
